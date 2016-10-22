cc48ccddc354b5711d2556d1520a6de336cb4a11
Veenith <danhnguyen782@gmail.com>
2016-10-22 14:53:00 -0700

    Set up Input and added Renderer Class

---
+++ Game Engine/EllixEngine/EllixEngine/Ellix.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace EllixEngine
+{
+    class Ellix
+    {
+        Renderer gameRender;
+        RenderWindow gameWindow;
+        public void Init() {
+            gameRender = new Renderer();
+            gameWindow = gameRender.init();
+            //Input
+            gameWindow.Closed += new EventHandler(OnClose);
+            gameWindow.KeyPressed += new EventHandler<KeyEventArgs>(KeyPressed);
+        }
+        //Input methods
+        void KeyPressed(object sender,KeyEventArgs e)
+        {
+            if (e.Code == Keyboard.Key.Escape) {
+                gameWindow.Close();
+            }
+        }
+        public bool checkWindowOpen() {
+            return gameWindow.IsOpen;
+        }
+
+        void OnClose(object sender, EventArgs e)
+        {
+            // Close the window when OnClose event is received
+            RenderWindow window = (RenderWindow) sender;
+            window.Close();
+        }
+        //GameEngine Methods
+        public void renderFrame() {
+            gameRender.frame(gameWindow);
+        }
+
+        public void getInput() {
+            gameWindow.DispatchEvents();
+        }
+
+        static void Main(string[] args)
+        {
+            Ellix gameEngine = new Ellix();
+            gameEngine.Init();
+            while (gameEngine.checkWindowOpen()) {
+                gameEngine.renderFrame();
+                gameEngine.getInput();
+            }
+        }
+    }
+}
--- Game Engine/EllixEngine/EllixEngine/Renderer.cs
+++ Game Engine/EllixEngine/EllixEngine/Renderer.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SFML;
 using SFML.Graphics;
 using SFML.Window;
 
 namespace EllixEngine
 {
-    class Program
+    class Renderer
     {
-        static void Main(string[] args)
+        Color clearColor = Color.White;
+        public RenderWindow init()
         {
             RenderWindow window = new RenderWindow(new VideoMode(1080, 760), "test", Styles.Default);
+            return window;
+        }
+        public void setClearColor(Color newColor) {
+            clearColor = newColor;
+        }
+
+        public void frame(RenderWindow window)
+        {
+            window.Clear(clearColor);
             window.Display();
         }
     }
